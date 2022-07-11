1da27dc90b372b97e13cc79fafc62b24d4accd13
devarrowsmith <devarrowsmith@gmail.com>
2022-07-11 22:40:42 +0100

    Update accel-poc-ios.

--- pages/accel-poc-ios/index.js
+++ pages/accel-poc-ios/index.js
@@ -15,18 +15,18 @@ function getMotionPermissions(){
             document.getElementById("beta").textContent = `${beta.toFixed(2)}`
             document.getElementById("gamma").textContent = `${gamma.toFixed(2)}`
 
-            if (alpha > 60 && alpha < 180) {
+            if (gamma < -70) {
               currentColumn = 1;
               document.getElementById("block").className = `column-${currentColumn}`;
             }
             
-            if (alpha >180 && alpha < 300) {
-              currentColumn = 3;
+            if (gamma > -70 && gamma < 70) {
+              currentColumn = 2;
               document.getElementById("block").className = `column-${currentColumn}`;
             }
             
-            if (alpha <60 || alpha > 300) {
-              currentColumn = 2;
+            if (gamma > 70) {
+              currentColumn = 3;
               document.getElementById("block").className = `column-${currentColumn}`;
             }
 
