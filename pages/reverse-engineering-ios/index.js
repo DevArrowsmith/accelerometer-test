af126d07dac8241f4367133e5e0ed56755d7a563
devarrowsmith <devarrowsmith@gmail.com>
2022-07-12 00:13:28 +0100

    Update reverse-engineering-ios page: More element variable tests.

--- pages/reverse-engineering-ios/index.js
+++ pages/reverse-engineering-ios/index.js
@@ -4,7 +4,7 @@ var vx = 0.0; // Velocity x and y
 var vy = 0.0;
 var updateRate = 1/60; // Sensor refresh rate
 
-let mod1 = document.getElementById("test-content-modification-arbitrary");
+const mod1 = document.getElementById("test-content-modification-arbitrary");
 
 function getAccel(){
   alert("getAccel invoked");
