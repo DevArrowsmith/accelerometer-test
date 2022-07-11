66ffe7724cc0b516f185ebf72f99ef38287dc174
devarrowsmith <devarrowsmith@gmail.com>
2022-07-11 22:29:27 +0100

    Update accel-poc-ios with the working implementation from accel-dot-test-ios.

--- pages/accel-poc-ios/index.js
+++ pages/accel-poc-ios/index.js
@@ -1,44 +1,37 @@
 let currentColumn = 2;
 
-//getAccel function: Tested, causes permissions access.
-function getAccel(){
+function getMotionPermissions(){
+    alert("getPermission invoked");
     DeviceMotionEvent.requestPermission().then(response => {
         alert(`Response: ${response}`);
         if (response == 'granted') {
-          window.addEventListener("deviceorientation", moveBlock);
+          // window.addEventListener("deviceorientation", moveBlock);
+          window.addEventListener('deviceorientation',(event) => {
+            let alpha = event.alpha;
+            let beta = event.beta;
+            let gamma = event.gamma;
+
+            document.getElementById("alpha").textContent = `${alpha.toFixed(2)}`
+            document.getElementById("beta").textContent = `${beta.toFixed(2)}`
+            document.getElementById("gamma").textContent = `${gamma.toFixed(2)}`
+
+            if (alpha > 60 && alpha < 180) {
+              currentColumn = 1;
+              document.getElementById("block").className = `column-${currentColumn}`;
+            }
+            
+            if (alpha >180 && alpha < 300) {
+              currentColumn = 3;
+              document.getElementById("block").className = `column-${currentColumn}`;
+            }
+            
+            if (alpha <60 || alpha > 300) {
+              currentColumn = 2;
+              document.getElementById("block").className = `column-${currentColumn}`;
+            }
+
+            document.getElementById("current-column").textContent = `${currentColumn}`;
+          });
         }
     });
 }
-
-// Event listener: Functions (Tested)
-document.getElementById("accelerometer-permissions").addEventListener("click", getAccel);
-
-
-function moveBlock(event) {  
-  if (event.gamma) {
-    document.getElementById("gamma").textContent = `${event.gamma.toFixed(2)}`
-  }
-  
-  if (event.beta) {
-    document.getElementById("beta").textContent = `${event.beta.toFixed(2)}`
-  }
-  
-	if (event.gamma < -16 && currentColumn > 1) {
-    currentColumn = 1;
-		document.getElementById("block").className = `column-${currentColumn}`;
-	}
-  
-  if (event.gamma > -16 && event.gamma < 16) {
-    currentColumn = 2;
-		document.getElementById("block").className = `column-${currentColumn}`;
-	}
- 
-  if (event.gamma > 16) {
-    currentColumn = 3;
-		document.getElementById("block").className = `column-${currentColumn}`;
-	}
-  
-  document.getElementById("current-column").textContent = `${currentColumn}`
-}
-  
-// window.addEventListener("deviceorientation", moveBlock);
