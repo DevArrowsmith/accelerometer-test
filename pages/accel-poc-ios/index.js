480a4fb8e1084ec3fe694b606e159adc569dbeab
devarrowsmith <devarrowsmith@gmail.com>
2022-07-12 00:35:46 +0100

    Update accel-poc-ios page: Implemeting findings from reverse-engineering-ios.

--- pages/accel-poc-ios/index.js
+++ pages/accel-poc-ios/index.js
@@ -2,7 +2,7 @@ let currentColumn = 2;
 
 const block = document.getElementById("block");
 const alphaReadout = document.getElementById("alpha");
-const betareadout = document.getElementById("beta");
+const betaReadout = document.getElementById("beta");
 const gammaReadout = document.getElementById("gamma");
 const columnReadout = document.getElementById("current-column")
 
@@ -11,14 +11,14 @@ function getMotionPermissions(){
     DeviceMotionEvent.requestPermission().then(response => {
         alert(`Response: ${response}`);
         if (response == 'granted') {
-          window.addEventListener('deviceorientation', (event) => {
+          window.addEventListener('deviceorientation',(event) => {
             alpha = event.alpha;
             beta = event.beta;
             gamma = event.gamma;
 
-            alphaReadout.textContent = `${alpha.toFixed(2)}`;
-            betaReadout.textContent = `${beta.toFixed(2)}`;
-            gammaReadout.textContent = `${gamma.toFixed(2)}`;
+            alphaReadout.textContent = `${alpha}`;
+            betaReadout.textContent = `${beta}`;
+            gammaReadout.textContent = `${gamma}`;
 
             // if (gamma < -70) {
             //   currentColumn = 1;
