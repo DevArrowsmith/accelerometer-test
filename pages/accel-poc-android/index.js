00a025dfa4488da123c11a156689b2ef70cb06d1
devarrowsmith <devarrowsmith@gmail.com>
2022-07-12 01:35:51 +0100

    Refactor accel-poc-android page. Stopped working after moving variables out of event listener & defining as const.

--- pages/accel-poc-android/index.js
+++ pages/accel-poc-android/index.js
@@ -1,40 +1,34 @@
 let currentColumn = 2;
 
-const block = document.getElementById("block");
-const alphaReadout = document.getElementById("alpha");
-const betaReadout = document.getElementById("beta");
-const gammaReadout = document.getElementById("gamma");
-const columnReadout = document.getElementById("current-column");
-
 function moveBlock(event) {  
   if (event.alpha) {
-    alphaReadout.textContent = `${event.alpha.toFixed(2)}`
-  }
-
-  if (event.beta) {
-    betaReadout.textContent = `${event.beta.toFixed(2)}`
+    document.getElementById("alpha").textContent = `${event.alpha.toFixed(2)}`
   }
 
   if (event.gamma) {
-    gammaReadout.textContent = `${event.gamma.toFixed(2)}`
+    document.getElementById("gamma").textContent = `${event.gamma.toFixed(2)}`
+  }
+  
+  if (event.beta) {
+    document.getElementById("beta").textContent = `${event.beta.toFixed(2)}`
   }
   
 	if (event.gamma < -70) {
     currentColumn = 1;
-		block.className = `column-${currentColumn}`;
+		document.getElementById("block").className = `column-${currentColumn}`;
 	}
   
   if (event.gamma > -70 && event.gamma < 70) {
     currentColumn = 2;
-		block.className = `column-${currentColumn}`;
+		document.getElementById("block").className = `column-${currentColumn}`;
 	}
  
   if (event.gamma > 70) {
     currentColumn = 3;
-		block.className = `column-${currentColumn}`;
+		document.getElementById("block").className = `column-${currentColumn}`;
 	}
   
-  columnReadout.textContent = `${currentColumn}`
+  document.getElementById("current-column").textContent = `${currentColumn}`
 }
   
 window.addEventListener("deviceorientation", moveBlock);
